ee81ed2c192b1329e29bc40b75612cd2b2cd7977
Donovant <Donovan@torgersonlabs.com>
2017-11-09 23:31:19 -0700

    Initial commit of project Euler problems 4-6

---
+++ C#/Project Euler - 4/Project Euler - 4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+ */
+namespace Project_Euler___4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int
+                current,
+                largest = 0,
+                max = 0,
+                rem,
+                reverse = 0;
+
+            for(int x = 999; x > 100; x--)
+            {
+                for(int y = 999; y > 100; y--)
+                {
+                    current = x * y;
+                    max = x * y;
+
+                    while (max > 0)
+                    {
+                        rem = max % 10;
+                        reverse = (reverse * 10) + rem;
+                        max = max / 10;
+                    }
+                    if (current == reverse && current > largest)
+                    {
+                        largest = current;
+                        break;
+                    }
+                    reverse = 0;
+                }
+            }
+            Console.WriteLine("{0}", largest);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Project Euler - 6/Project Euler - 6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+The sum of the squares of the first ten natural numbers is,
+
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+namespace Project_Euler___6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double
+                difference = 0,
+                square_of_sums = 0,
+                sum_of_squares = 0;
+
+            for(double x = 1; x < 101; x++)
+            {
+                sum_of_squares += Math.Pow(x, 2);
+                square_of_sums += x;
+            }
+            square_of_sums = Math.Pow(square_of_sums, 2);
+            difference = square_of_sums - sum_of_squares;
+            Console.WriteLine("{0}", difference);
+            Console.ReadLine();
+        }
+    }
+}
