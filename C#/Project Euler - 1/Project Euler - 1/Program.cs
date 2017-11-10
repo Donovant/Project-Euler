9559fccecd05b839b2a12a9db98335a585419ee1
Donovant <Donovan@torgersonlabs.com>
2017-11-09 22:45:56 -0700

    Initial commit of project Euler problems 1-3

---
+++ C#/Project Euler - 1/Project Euler - 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler___1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = 0;
+            for(int x = 0; x < 1000; x++)
+            {
+                if(x%3 == 0 || x%5 == 0)
+                {
+                    sum += x;
+                }
+            }
+            Console.WriteLine("\nsum: {0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Project Euler - 3/Project Euler - 3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+ */
+namespace Project_Euler___3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int64 x = 600851475143;
+            Int64 nextFactor = 2;
+            Int64 lpf = 0;
+            
+            while (x > 1)
+            {
+                if (x % nextFactor > 0)
+                {
+                    nextFactor = 3;
+                    do
+                    {
+                        if (x % nextFactor == 0)
+                        {
+                            break;
+                        }
+
+                        nextFactor += 2;
+                    } while (nextFactor < x);
+                }
+
+                x /= nextFactor;
+                if (nextFactor > lpf)
+                {
+                    lpf = nextFactor;
+                }
+            }
+
+
+            Console.WriteLine("{0}", lpf);
+            Console.ReadLine();
+        }
+
+
+    }
+}
